a62b34cc40b866423b2b7b1b243ef7a2c82bf3e6
Shivam <mittalshivam440@gmail.com>
2024-04-30 19:51:50 +0530

    second intialization

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -22,7 +22,7 @@ const getProductById = async (req, res) => {
 const createProduct = async (req, res) => {
     try {
         const product = await Product.create(req.body);
-        res.status(200).json(product);
+        res.status(201).json(product);
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
